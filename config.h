fd309b181f293106d188c4db5ce19e6cfb9eacac
istyak <irana@odduu.com>
2017-04-26 13:35:32 +0600

    dwm with no status bar

--- config.h
+++ config.h
@@ -21,7 +21,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7" };
+static const char *tags[] = { "1" };
  
 static const char pp_moni[]  = "pdfpc - presenter";
 static const char pp_proj[]  = "pdfpc - presentation";
@@ -62,12 +62,6 @@ static const Layout layouts[] = {
 #define MODKEY Mod4Mask
 #define TK(KEY,TAG) \
 	{ KeyPress, MODKEY,                         KEY,  view,        {.ui = 1 << TAG} }, \
-	{ KeyPress, MODKEY|ShiftMask,               KEY,  toggleview,  {.ui = 1 << TAG} }, \
-	{ KeyPress, MODKEY|ControlMask,             KEY,  tag,         {.ui = 1 << TAG} }, \
-	{ KeyPress, MODKEY|ControlMask|ShiftMask,   KEY,  toggletag,   {.ui = 1 << TAG} },
-#define RK(MASK,KEY,ACTION) \
-	{ KeyPress, MASK,                           KEY,  ACTION,      {.i  = +1 } }, \
-	{ KeyPress, MASK|ShiftMask,                 KEY,  ACTION,      {.i  = -1 } },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
@@ -83,66 +77,13 @@ static const char *passmenu[]         = { "passmenu", "-i", "-fn", dmenufont, "-
 
 static Key keys[] = {
   TK(                     XK_i,            0               )
-  TK(                     XK_o,            1               )
-  TK(                     XK_p,            2               )
-  TK(                     XK_k,            3               )
-  TK(                     XK_l,            4               )
-  TK(                     XK_semicolon,    5               )
-  TK(                     XK_apostrophe,   6               )
-  RK( MODKEY,             XK_comma,        focusstack      )
-  RK( MODKEY|ControlMask, XK_comma,        pushstack       )
-  RK( MODKEY,             XK_period,       focusmon        )
-  RK( MODKEY|ControlMask, XK_period,       tagmon          )
-
-	{ KeyPress,   MODKEY,             XK_1,            spawn,          {.v = dmenucmd } },
-	{ KeyPress,   MODKEY,             XK_2,            spawn,          {.v = passmenu } },
-	{ KeyPress,   MODKEY,             XK_3,            spawn,          {.v = clipmenu } },
-	{ KeyPress,   MODKEY,             XK_Return,       spawn,          SHCMD("urxvtc -title Terminal") },
-	{ KeyPress,   MODKEY|ShiftMask,   XK_Return,       spawn,          SHCMD("urxvtc -title Scratch -geometry 160x40") },
-	{ KeyPress,   MODKEY|ControlMask, XK_t,            setlayout,      {.v = &layouts[0] } },
-	{ KeyPress,   MODKEY|ControlMask, XK_b,            setlayout,      {.v = &layouts[3] } },
-	{ KeyPress,   MODKEY|ControlMask, XK_h,            setlayout,      {.v = &layouts[4] } },
-	{ KeyPress,   MODKEY|ShiftMask,   XK_9,            setmfact,       {.f = -0.05} },
-	{ KeyPress,   MODKEY|ShiftMask,   XK_0,            setmfact,       {.f = +0.05} },
-	{ KeyPress,   MODKEY|ShiftMask,   XK_r,            setmfact,       {.f = 1.5} },  /* >1.0 sets absolute value */
-	{ KeyPress,   MODKEY,             XK_backslash,    spawn,          SHCMD("firefox") },
-	{ KeyPress,   MODKEY|ControlMask, XK_backslash,    spawn,          SHCMD("firefox --private-window") },
-	{ KeyPress,   MODKEY|ShiftMask,   XK_backslash,    spawn,          SHCMD("tor-browser-en") },
-	{ KeyPress,   MODKEY,             XK_slash,        spawn,          {.v = mpdmenu_library } },
-	{ KeyPress,   MODKEY|ControlMask, XK_slash,        spawn,          {.v = mpdmenu_playlist } },
-	{ KeyPress,   MODKEY,             XK_Prior,        spawn,          SHCMD("pulseaudio-ctl up") },
-	{ KeyPress,   MODKEY,             XK_Next,         spawn,          SHCMD("pulseaudio-ctl down") },
-	{ KeyPress,   MODKEY,             XK_m,            spawn,          SHCMD("pulseaudio-ctl mute") },
-	{ KeyPress,   MODKEY,             XK_Down,         spawn,          SHCMD("mpc toggle") },
-	{ KeyPress,   MODKEY,             XK_Right,        spawn,          SHCMD("mpc next") },
-	{ KeyPress,   MODKEY,             XK_Left,         spawn,          SHCMD("mpc prev") },
-	{ KeyRelease, MODKEY|ControlMask, XK_F10,          spawn,          SHCMD("screenshot --focused") },
-	{ KeyRelease, MODKEY,             XK_F10,          spawn,          SHCMD("screenshot --all") },
-	{ KeyRelease, False,              XK_F10,          spawn,          SHCMD("screenshot --select") },
-	{ KeyPress,   MODKEY,             XK_Delete,       spawn,          SHCMD("slock") },
-	{ KeyPress,   MODKEY,             XK_Delete,       spawn,          SHCMD("sleep 1; xset dpms force off") },
-	{ KeyPress,   MODKEY,             XK_t,            spawn,          SHCMD("trello-popup") },
-	{ KeyPress,   MODKEY,             XK_d,            spawn,          SHCMD("notify-send \"$(world-tz)\"") },
-	{ KeyPress,   MODKEY,             XK_s,            spawn,          SHCMD("xinput-toggle -r yubikey -n -e -t 10") },
-	{ KeyPress,   MODKEY,             XK_Tab,          zoom,           {0} },
-	{ KeyPress,   MODKEY,             XK_BackSpace,    killclient,     {0} },
 	{ KeyPress,   MODKEY|ShiftMask,   XK_space,        togglefloating, {0} },
-	{ KeyPress,   Mod1Mask|ShiftMask, XK_q,            quit,           {0} },
 };
 
 /* button definitions */
 /* click can be ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
 
--- dwm.c
+++ dwm.c
@@ -2217,7 +2217,7 @@ void
 updatestatus(void)
 {
 	if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext)))
-		strcpy(stext, "dwm-"VERSION);
+		strcpy(stext, "installation");
 	drawbar(selmon);
 }
 
